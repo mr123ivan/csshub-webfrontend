f7981a3fc7d96d2faa202d976ac49c1079aa6cc0
Maggins08 <165860874+Maggins08@users.noreply.github.com>
2025-03-23 23:03:01 +0800

    Add files via upload

---
+++ backend/src/main/java/edu/cit/csshub/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package edu.cit.csshub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("CCSHUB IS RUNNING!");
+	}
+
+}
